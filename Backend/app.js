e473198a98f0fb7dcb268734bc82e1611559a92f
Suman <sumangayen831@gmail.com>
2025-04-24 07:26:06 +0530

    basic setup

---
+++ Backend/app.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv"
+dotenv.config();
+import cors from "cors"
+import express from "express";
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+
+app.get("/", (req, res) => {
+    res.send("Server is working")
+})
+
+export {app}
---
+++ Backend/server.js
@@ -0,0 +1,13 @@
+import http from "http"
+import {app} from "./app.js"
+
+
+const server = http.createServer(app)
+
+
+
+
+server.listen(3000, () => {
+    console.log("server is runing at port 3000");
+    
+})
